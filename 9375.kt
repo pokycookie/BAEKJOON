995836498c4e1a66c452317a96e20345680fc284
pokycookie <0cookieboy0@gmail.com>
2023-09-19 17:55:36 +0900

    Update 9375.kt

--- 9375.kt
+++ 9375.kt
@@ -13,7 +13,10 @@ fun main() = with(br) {
         }
 
         var ans = 1
+        // 각 의상 종류마다 가지고 있는 의상수에 + 1을 한 값을 모아 서로 곱함
+        // 추가된 1개의 경우는 해당 의상 종류를 입지 않는 경우
         for (key in map.keys) ans *= map[key]!!.size + 1
+        // 이렇게 만들어진 결과는 아무것도 입지 않는 경우도 포함하므로, 그 경우 1개를 제외하여 답을 구함
         bw.write((ans - 1).toString())
         bw.newLine()
     }
