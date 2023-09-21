341b8ac866bb139ee62fbced7b09d1f99ef89066
pokycookie <0cookieboy0@gmail.com>
2023-09-21 17:51:55 +0900

    Update 1389.kt

--- 1389.kt
+++ 1389.kt
@@ -1,44 +1,38 @@
+// use Floyd-Warshall
+
 val br = System.`in`.bufferedReader()
+val bw = System.out.bufferedWriter()
 
 fun main() = with(br) {
     val (n, m) = readLine().split(" ").map { it.toInt() }
-    val graph = Array(n + 1) { mutableListOf<Int>() }
+    val graph = Array(n) { IntArray(n) { 999 } }
 
+    for (i in 0 until n) graph[i][i] = 0
     repeat(m) {
         val (a, b) = readLine().split(" ").map { it.toInt() }
-        graph[a].add(b)
-        graph[b].add(a)
+        graph[a - 1][b - 1] = 1
+        graph[b - 1][a - 1] = 1
     }
 
-    fun bfs(start: Int): Int {
-        val queue = ArrayDeque<Pair<Int, Int>>()
-        val visited = Array(n + 1) { false }
-        var acc = 0
-
-        queue.addLast(start to 0)
-        visited[start] = true
-        while (queue.isNotEmpty()) {
-            val (vertex, depth) = queue.removeFirst()
-            acc += depth
-
-            for (child in graph[vertex]) {
-                if (visited[child]) continue
-                visited[child] = true
-                queue.addLast(child to (depth + 1))
+    for (waypoint in 0 until n) {
+        for (start in 0 until n) {
+            for (end in 0 until n) {
+                graph[start][end] = minOf(graph[start][end], graph[start][waypoint] + graph[waypoint][end])
             }
         }
-
-        return acc
     }
 
     var ans = 0
     var min = Int.MAX_VALUE
-    for (vertex in 1 .. n) {
-        val acc = bfs(vertex)
+    for (vertex in 0 until n) {
+        val acc = graph[vertex].sum()
         if (acc < min) {
-            ans = vertex
+            ans = vertex + 1
             min = acc
         }
     }
-    println(ans)
+
+    bw.write(ans.toString())
+    bw.flush()
+    bw.close()
 }
