37a7b8d768baa3d5906b76a6a1be43a667b255fe
pokycookie <0cookieboy0@gmail.com>
2023-10-27 11:22:06 +0900

    Create 11376.java

---
+++ 11376.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static List<Integer>[] path;
+    private static int[] dest;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        path = new List[n];
+        dest = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            path[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            dest[i] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            while (s-- > 0) {
+                int work = Integer.parseInt(st.nextToken());
+                path[i].add(work - 1);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            visited = new boolean[m];
+            if (matching(i)) {
+                ans++;
+            }
+            visited = new boolean[m];
+            if (matching(i)) {
+                ans++;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    private static boolean matching(int start) {
+        for (int end : path[start]) {
+            if (visited[end]) {
+                continue;
+            }
+            visited[end] = true;
+            if (dest[end] == -1 || matching(dest[end])) {
+                dest[end] = start;
+                return true;
+            }
+        }
+        return false;
+    }
+}
