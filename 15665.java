769fb3dc1f56439f35e6c8cbade41ccfe1719b30
pokycookie <0cookieboy0@gmail.com>
2023-12-23 19:48:28 +0900

    Create 15665.java

---
+++ 15665.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int n, m;
+    static List<Integer> sequence;
+    static Stack<String> stack = new Stack<>();
+    static Set<String> set = new HashSet<>();
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st =  new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        sequence = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .sorted()
+                .collect(Collectors.toList());
+
+        backtracking();
+        bw.flush();
+        bw.close();
+    }
+
+    static void backtracking() throws IOException {
+        if (stack.size() == m) {
+            String stackString = String.join(" ", stack);
+            if (!set.contains(stackString)) {
+                bw.write(stackString);
+                bw.newLine();
+                set.add(stackString);
+            }
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            stack.push(Integer.toString(sequence.get(i)));
+            backtracking();
+            stack.pop();
+        }
+    }
+}
